79ac09c7678dbc54854f8dcffdf2a512801b3cd7
iamssa <alexandrsimakovv@mail.ru>
2019-10-11 17:57:42 +0500

    Add homework Lesson6

---
+++ JavaScript/JavaScript-4/JavaScript-4.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const path = require('path')
+const base = 'foo/'
+ 
+const readFile = (dir, cb) => {
+
+  let results = {dirs: [], files: []}
+
+  fs.readdir(dir, (err, items) => {
+    if (err)  return cb(err) 
+ 
+    var itemList = items.length
+    if (!itemList){ return cb(null, results)}
+    items.forEach((file) => {
+      file = path.join(dir, file)
+      fs.stat(file, (err, stat) => {
+        if (stat && stat.isDirectory()) {
+          results.dirs.push(file)  
+          readFile(file, (err, res) => {
+            results.files = results.files.concat(res.files)
+            results.dirs = results.dirs.concat(res.dirs)
+            if (!--itemList) cb(null, results)
+          })
+        } else {
+          results.files.push(file)
+          if (!--itemList) cb(null, results)
+        }
+      })
+    })
+  })
+}
+ 
+
+readFile((base), (err, results) => {
+  if (err) throw err;
+  console.log(JSON.stringify(results));
+});
